cd1ade4fe514e76b676825ceb218b858380418e6
marciocarv <marciotibapa@gmail.com>
2020-09-10 18:03:45 -0300

    salvo

--- instancias/script.js
+++ instancias/script.js
@@ -5,22 +5,3 @@ const vm = new Vue({
         text: 'Texto da segunda instância'
     }
 });
-
-const helloWorldComponent = {
-    template: `
-    <div class="component">
-        Hello, World!
-    </div>
-    `
-}
-
-const vm2 = new Vue({
-    el: '#app2',
-    data:{
-        title: 'Instância 2',
-        text: 'Texto da segunda instância'
-    },
-    components: {
-        'hello-world': helloWorldComponent,
-    },
-});
