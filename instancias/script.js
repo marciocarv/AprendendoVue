de4f0e7738635009b1a44469d7bf89ce18e86306
marciocarv <marciotibapa@gmail.com>
2020-09-15 07:47:42 -0300

    urna eletronica

--- instancias/script.js
+++ instancias/script.js
@@ -1,8 +1,22 @@
 const vm = new Vue({
     el: '#app',
     data: {
-      text: 'Lorem ipsum dolor sit amet consectetur',
-      showText: true,
-      textClass: 'red',
+      users: {
+        daniel: 'admin',
+        tiago: 'editor',
+        joao: 'guest',
+      },
+      todos: [
+        {
+          id: 123,
+          title: 'Wsh my car',
+          complete: false,
+        },
+        {
+          id: 321,
+          title: 'Buy some milk',
+          complete: true,
+        }
+      ]
     }
   })
