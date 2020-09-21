ce1b76e0575bdcce4aa86187e411e6a606ba0175
= <marciotibapa@gmail.com>
2020-09-20 23:59:09 -0300

    atualizacao

---
+++ instancias/etapas.js
@@ -0,0 +1,78 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '11111',
+                name: 'José Aparecido',
+                partido: 'PT do C',
+                fotos:[
+                    {url: '11111.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '22222',
+                name: 'Mauricio Lopes',
+                partido: 'PSD',
+                fotos:[
+                    {url: '22222.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '33333',
+                name: 'Luciano Marques',
+                partido: 'PPGF',
+                fotos:[
+                    {url: '22222.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '44444',
+                name: 'Paulo Carneiro',
+                partido: 'TTED',
+                fotos:[
+                    {url: '44444.jpg', legenda: 'Vereador', small: false}
+                ]
+            }
+        ]
+
+    },
+    {
+        titulo: "PREFEITO",
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '22',
+                name: 'Cinthia Ribeiro',
+                partido: 'PT do C',
+                vice: 'Joao Campos',
+                fotos:[
+                    {url: '22.jpg', legenda: 'Prefeito', small: false},
+                    {url: '22_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '18',
+                name: 'Junior Geo',
+                partido: 'PSD',
+                vice: 'Joaquim Maia',
+                fotos:[
+                    {url: '18.jpg', legenda: 'Prefeito', small: false},
+                    {url: '18_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '37',
+                name: 'Tiago Andrino',
+                partido: 'PPGF',
+                vice: 'Maria Alves',
+                fotos:[
+                    {url: '37.jpg', legenda: 'Prefeito', small: false},
+                    {url: '37_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+
+    }
+]
--- instancias/script.js
+++ instancias/script.js
@@ -1,4 +1,93 @@
-const vm = new Vue({
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3');
+
+let etapaAtual = 0;
+let numero = '';
+
+function comecarEtapa(){
+  let etapa = etapas[etapaAtual];
+
+  let numeroHtml = '';
+
+  for(let i=0;i<etapa.numeros;i++){
+    if(i===0){
+      numeroHtml += '<div class="numero pisca"></div>';
+    }else{
+      numeroHtml += '<div class="numero"></div>';
+    }
+  }
+
+  seuVotoPara.style.display = 'none';
+  cargo.innerHTML = etapa.titulo;
+  descricao.innerHTML = '';
+  aviso.style.display = 'none';
+  lateral.innerHTML = '';
+  numeros.innerHTML = numeroHtml;
+}
+
+function atualizaInterface(){
+  let etapa = etapas[etapaAtual];
+  let candidato = etapa.candidatos.filter((item)=>{
+    if(item.numero === numero){
+      return true;
+    }else{
+      return false;
+    }
+  });
+
+  if(candidato.length > 0){
+    candidato = candidato[0];
+    seuVotoPara.style.display = 'block';
+    aviso.style.display = 'block';
+    descricao.innerHTML = `Nome: ${candidato.name}<br />Partido: ${candidato.partido}`;
+
+    let fotosHtml = '';
+
+    for(let i in candidato.fotos){
+      fotosHtml += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}">${candidato.fotos[i].legenda}</div>`;
+    }
+    
+    lateral.innerHTML = fotosHtml;
+  }
+}
+
+function clicou(n){
+  let elNumero = document.querySelector('.numero.pisca');
+  if(elNumero !== null){
+    elNumero.innerHTML = n;
+    numero = `${numero}${n}`;
+
+    elNumero.classList.remove('pisca');
+    if(elNumero.nextElementSibling!==null){
+      elNumero.nextElementSibling.classList.add('pisca');
+    }else{
+      atualizaInterface();
+    }
+    
+  }
+}
+
+function branco(){
+  alert("clicou em branco");
+}
+
+function confirma(){
+  alert("clicou em  confirma");
+}
+
+function corrige(){
+  alert("clicou em corrige");
+}
+
+comecarEtapa();
+
+
+
+/*const vm = new Vue({
     el: '#app',
     data: {
       users: {
@@ -20,3 +109,6 @@ const vm = new Vue({
       ]
     }
   })
+  
+  <script src="https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js"></script>
+  */
